09237899e085e22a39523915159de285cf63b0a0
njagua <njagua.gma@gmail.com>
2024-08-30 07:41:55 +0300

    add

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    # Initialize the first two Fibonacci numbers
+    fib_sequence = [0, 1]
+    
+    # Generate the Fibonacci sequence up to the desired length
+    for i in range(2, length):
+        next_value = fib_sequence[i - 1] + fib_sequence[i - 2]
+        fib_sequence.append(next_value)
+    
+    # Print the Fibonacci sequence
+    print(fib_sequence[:length])
+
+# Example usage:
+print_fibonacci(9)
